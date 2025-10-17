3747e1c44818f4c7e2910884074a32ed103a4113
skpeterson <skpet@unc.edu>
2025-10-17 17:08:38 -0400

    updated visualization of bispecific t cell co culture figure with new bispecific data

--- scripts/R/bispecific_coculture_visualization.R
+++ scripts/R/bispecific_coculture_visualization.R
@@ -9,6 +9,7 @@ library(ggplot2)
 
 t2_data  <- read.csv('work/data/bispecific_cell_culture/PRAME_T2_peptide_pulse.txt', check.names = F, sep = '\t')
 bispecific_time_data <- read.csv('work/data/bispecific_cell_culture/nut_cell_lines_bispecific_luminescence.csv', check.names = F)
+bispecific_data_2 <- read.csv('work/data/bispecific_cell_culture/nut_cell_lines_bispecific_lumiescence_expt2.csv', check.names = F)
 conc <- read.csv('work/data/bispecific_cell_culture/PRAME_cytotoxicity_concentration_gradient.csv', check.names = F, sep=',')
 
 
@@ -89,7 +90,7 @@ t2_cell_viability <- ggplot() +
 ggsave('work/results/bispecific/Figure5b_T2_norm_cell_viability_concentration.pdf', plot = t2_cell_viability, units = 'in', width = 4.5, height = 3.5,  dpi = 500)
 
 
-# Panel C Cytoxicity - Concentration Concentration ------------------------------------------
+# Panel C Cytoxicity - Concentration ------------------------------------------
 
 conc <- conc %>% mutate(Concentration = 10^seq(-8, -12, length.out = n()))
 
@@ -177,6 +178,164 @@ NCI_cells_lines_plot <- ggplot() +
   )
   
 ggsave('work/results/bispecific/Figure5c_NCI_celllines_norm_cell_viability_dose_titration.pdf', plot = NCI_cells_lines_plot,units = 'in', width = 4.5, height = 3.5, dpi = 500)
+
+
+
+# NUT carcinoma cell viability after bispecific treatment -----------------
+
+# Calculate the mean and standard deviation of each "no bispecific" condition
+control_summary <- bispecific_data_2 %>%
+  filter(bispecific_condition == "None") %>%
+  group_by(culture_condition, cell_line) %>%
+  summarise(mean = mean(relative_luminescence), 
+                        sd = sd(relative_luminescence),
+            .groups = 'drop')
+
+test_conditions <- bispecific_data_2 %>% filter(bispecific_condition %in% c('PRAME', 'NON-SPECIFIC'))
+
+# Normalize the PRAME and NON-SPECIFIC bispecific conditions to the NONE control
+data_normalized <- test_conditions %>%
+  left_join(control_summary, 
+            by = c("culture_condition", "cell_line")) %>%
+  mutate(fold_change_lum = relative_luminescence / mean  )
+
+
+# Aggregate to calculate mean and SD for each group
+data_summary <- data_normalized %>%
+  group_by(culture_condition, cell_line, bispecific_condition) %>%
+  summarise(
+    mean_luminescence = mean(fold_change_lum, na.rm = TRUE),
+    sd_luminescence = sd(fold_change_lum, na.rm = TRUE),
+    .groups = "drop"
+  )
+
+data_normalized %>% group_by(culture_condition, cell_line) %>% t.test()
+
+data_summary$cell_line <- factor(data_summary$cell_line, levels = c('10-15', '14169','PER403','JCM1','TC-797'))
+
+ggplot(data_summary, aes(x = as.factor(culture_condition), y = mean_luminescence, color = as.factor(bispecific_condition))) +
+  # Add points for the mean
+  geom_point(size = 3) +
+  # Add error bars for SD
+  geom_errorbar(aes(ymin = mean_luminescence - sd_luminescence, 
+                    ymax = mean_luminescence + sd_luminescence),
+                width = 0.2, size = 0.8) +
+  # Facet grid by cell line
+  facet_grid(~cell_line, scales = "fixed", 
+             labeller = labeller(
+               cell_line = c(
+                 "PER403" = "PER403 \n (PRAME +, A*02 +)",
+                 "14169" = "14169 \n (PRAME +, A*02 +)",
+                 "10-15" = "10-15 \n (PRAME +, A*02 +)",
+                 "TC-797" = "TC-797 \n (PRAME -, A*02 +)",
+                 "JCM1" = "JCM1 \n (PRAME +, A*02 -)"
+               )
+             )) +
+  # Logarithmic fold change and custom aesthetics
+  theme_bw() +
+  labs(
+    title = "",
+    y = "Luminescence Fold Change \n(BiTE/No BiTE)",
+    x = "",
+    color = ""
+  ) +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    plot.title = element_text(hjust = 0.5),
+    panel.spacing = unit(0.5, "lines"),
+    panel.grid.minor = element_blank(),
+    panel.grid.major = element_blank()
+  ) +
+  scale_x_discrete(
+    labels = c(
+      "0:1 E:T" = "0:1 E:T",
+      "1:1 E:T" = "1:1 E:T",
+      "5:1 E:T" = "5:1 E:T"
+    )
+  ) +
+  geom_hline(yintercept = 1, linetype = "dashed", color = "gray50")
+
+
+t_results <- data_normalized %>%
+  filter(culture_condition != '0:1 E:T') %>% 
+  group_by(cell_line, culture_condition) %>%
+  summarise(
+    p_value = t.test(relative_luminescence ~ bispecific_condition)$p.value,
+    .groups = "drop"
+  )
+
+
+
+# look at the impact of T cell culture ratio ------------------------------
+
+control_summary <- bispecific_data_2 %>%
+  filter(bispecific_condition == "None", culture_condition == '0:1 E:T') %>%
+  group_by(culture_condition, cell_line) %>%
+  summarise(mean = mean(relative_luminescence), 
+            sd = sd(relative_luminescence),
+            .groups = 'drop')
+
+test_conditions <- bispecific_data_2 %>% filter(bispecific_condition == 'None', culture_condition != '0:1 E:T')
+
+# Normalize the PRAME and NON-SPECIFIC bispecific conditions to the NONE control
+data_normalized <- test_conditions %>%
+  left_join(control_summary, 
+            by = "cell_line") %>%
+  mutate(fold_change_lum = relative_luminescence / mean  )
+
+
+# Aggregate to calculate mean and SD for each group
+data_summary <- data_normalized %>%
+  group_by(cell_line, culture_condition.x) %>%
+  summarise(
+    mean_luminescence = mean(fold_change_lum, na.rm = TRUE),
+    sd_luminescence = sd(fold_change_lum, na.rm = TRUE),
+    .groups = "drop"
+  )
+
+data_summary$cell_line <- factor(data_summary$cell_line, levels = c('10-15', '14169','PER403','JCM1','TC-797'))
+
+ggplot(data_summary, aes(x = as.factor(culture_condition.x), y = mean_luminescence)) +
+  # Add points for the mean
+  geom_point(size = 3) +
+  # Add error bars for SD
+  geom_errorbar(aes(ymin = mean_luminescence - sd_luminescence, 
+                    ymax = mean_luminescence + sd_luminescence),
+                width = 0.2, size = 0.8) +
+  # Facet grid by cell line
+  facet_grid(~cell_line, scales = "fixed", 
+             labeller = labeller(
+               cell_line = c(
+                 "PER403" = "PER403 \n (PRAME +, A*02 +)",
+                 "14169" = "14169 \n (PRAME +, A*02 +)",
+                 "10-15" = "10-15 \n (PRAME +, A*02 +)",
+                 "TC-797" = "TC-797 \n (PRAME -, A*02 +)",
+                 "JCM1" = "JCM1 \n (PRAME +, A*02 -)"
+               )
+             )) +
+  # Logarithmic fold change and custom aesthetics
+  theme_bw() +
+  labs(
+    title = "",
+    y = "Luminescence Fold Change \n(E:T Ratio/Tumor Cells Only)",
+    x = "",
+    color = ""
+  ) +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    plot.title = element_text(hjust = 0.5),
+    panel.spacing = unit(0.5, "lines"),
+    panel.grid.minor = element_blank(),
+    panel.grid.major = element_blank()
+  ) +
+  scale_x_discrete(
+    labels = c(
+      "1:1 E:T" = "1:1 E:T",
+      "5:1 E:T" = "5:1 E:T"
+    )
+  ) +
+  geom_hline(yintercept = 1, linetype = "dashed", color = "gray50")
+
 
 # Panel D - NUT carcinoma Cell viability after bispecific treatment  ---------------------------------------------------
 
